ff3afd0b45a98729d64bd83fdb658f11a3233715
root <root@2.j3ckin.de>
2019-08-06 07:38:03 +0200

    update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -r /etc/EasyCMD/update/
 rm /bin/easycmd
 rm /bin/cmd
 git clone https://github.com/jeckin/EasyCMD /etc/EasyCMD/update
 chmod +x /etc/EasyCMD/update/install.sh
 /etc/EasyCMD/update/install.sh
+rm -r /etc/EasyCMD/update
